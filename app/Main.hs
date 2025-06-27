efcd4ba345395fcb405bbd9790fcb6c92f4b4468
Norbert Dzikowski <lambdax.one@icloud.com>
2025-06-27 16:52:44 +0200

    app: use optparse-applicative for better CLI experience

--- app/Main.hs
+++ app/Main.hs
@@ -3,25 +3,68 @@ module Main (main) where
 
 import qualified Backend.Python as Python
 import qualified Convex.Parser as P
-import System.Environment (getArgs)
+import Data.List (intercalate)
+import Options.Applicative
 import System.Exit (exitFailure)
+
+data GenTarget = Python | Rust deriving (Show, Eq, Enum, Bounded)
+
+availableTargets :: String
+availableTargets = "Available: " ++ intercalate ", " (map show [minBound :: GenTarget .. maxBound :: GenTarget])
+
+instance Read GenTarget where
+  readsPrec _ "Python" = [(Python, "")]
+  readsPrec _ "Rust" = [(Rust, "")]
+  readsPrec _ _ = []
+
+data CliOptions = CliOptions
+  { schemaPath :: FilePath,
+    declarationsDir :: FilePath,
+    target :: GenTarget
+  }
+
+cliOptionsParser :: Parser CliOptions
+cliOptionsParser =
+  CliOptions
+    <$> strOption
+      ( long "schema"
+          <> metavar "SCHEMA_PATH"
+          <> help "Path to the source schema.ts file"
+      )
+    <*> strOption
+      ( long "declarations"
+          <> metavar "DECL_DIR"
+          <> help "Path to the root of the tsc-generated declarations directory"
+      )
+    <*> option
+      auto
+      ( long "target"
+          <> metavar "TARGET"
+          <> value Python
+          <> help ("Target language for code generation " ++ availableTargets)
+      )
 
 main :: IO ()
 main = do
-  args <- getArgs
-  case args of
-    [schemaPath, declRootDir] -> do
-      -- Run the main orchestrator parser
-      projectResult <- P.parseProject schemaPath declRootDir
-      case projectResult of
-        Left err -> do
-          putStrLn $ "Error parsing project: " ++ err
-          exitFailure
-        Right project -> do
-          -- Pass the complete project AST to the Python backend
-          let pythonCode = Python.generatePythonCode project
-          -- Write the final result to `convex.py`
-          writeFile "convex.py" pythonCode
-    _ -> do
-      putStrLn "Usage: convex-schema-parser <path-to-schema.ts> <path-to-declarations-root>"
+  opts <- customExecParser p optsParserInfo
+  projectResult <- P.parseProject (schemaPath opts) (declarationsDir opts)
+  case projectResult of
+    Left err -> do
+      putStrLn $ "Error parsing project: " ++ err
       exitFailure
+    Right project ->
+      case target opts of
+        Python -> do
+          let pythonCode = Python.generatePythonCode project
+          putStrLn pythonCode
+        Rust ->
+          putStrLn "Rust backend not yet implemented."
+  where
+    optsParserInfo =
+      info
+        (cliOptionsParser <**> helper)
+        ( fullDesc
+            <> progDesc "Generate typed clients from a Convex schema and function definitions."
+            <> header "convex-parser - A code generator for Convex"
+        )
+    p = prefs showHelpOnError
