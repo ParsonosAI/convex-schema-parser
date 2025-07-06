eecf74fdb1666e040938b86f8d01ea607d4146da
Norbert Dzikowski <lambdax.one@icloud.com>
2025-07-06 16:19:57 +0200

    test: Add another test case for complex FromConvexValue serde

--- test/RustSerializationTest.hs
+++ test/RustSerializationTest.hs
@@ -64,7 +64,7 @@ complexCreateFunction =
 expectedComplexSerializationImpl :: String
 expectedComplexSerializationImpl =
   unlines
-    [ "impl types::CreateComplexDocPayloadObject {",
+    [ "impl CreateComplexDocPayloadObject {",
       "    pub fn to_convex_value(&self) -> Result<Value, ApiError> {",
       "        let mut btmap = BTreeMap::new();",
       "        if let Some(v) = &self.name {",
@@ -79,6 +79,102 @@ expectedComplexSerializationImpl =
       "}"
     ]
 
+getAssetsFunction :: Action.ConvexFunction
+getAssetsFunction =
+  Action.ConvexFunction
+    { Action.funcName = "getAssets",
+      Action.funcPath = "functions/assets",
+      Action.funcType = Action.Query,
+      Action.funcArgs = [("projectId", Schema.VId "projects")],
+      Action.funcReturn =
+        Schema.VArray
+          ( Schema.VObject
+              [ ("_id", Schema.VId "assets"),
+                ("_creationTime", Schema.VNumber),
+                ("project_id", Schema.VId "projects"),
+                ("asset_name", Schema.VString),
+                ( "link_metadata",
+                  Schema.VObject
+                    [ ("summary", Schema.VBytes),
+                      ("sample_rate", Schema.VInt64),
+                      ("length", Schema.VInt64)
+                    ]
+                ),
+                ("asset_essence_mtime", Schema.VInt64)
+              ]
+          )
+    }
+
+expectedGetAssetsSerializationImpl :: String
+expectedGetAssetsSerializationImpl =
+  unlines
+    [ "    impl TryFrom<Value> for GetAssetsReturnObject {",
+      "        type Error = ApiError;",
+      "        fn try_from(value: Value) -> Result<Self, Self::Error> {",
+      "            let obj = match value {",
+      "                Value::Object(map) => map,",
+      "                _ => return Err(ApiError::ConvexClientError(\"Expected object\".to_string())),",
+      "            };",
+      "            fn get__id(map: &BTreeMap<String, Value>, key: &str) -> Result<Id<types::AssetsDoc>, ApiError> {",
+      "                match map.get(key) {",
+      "                    Some(v) => {",
+      "                        Ok(FromConvexValue::from_convex(v.clone())?)",
+      "                    }",
+      "                    _ => return Err(ApiError::ConvexClientError(format!(\"Expected field (Id<types::AssetsDoc>) '{}' not found\", key))),",
+      "                }",
+      "            }",
+      "            fn get__creation_time(map: &BTreeMap<String, Value>, key: &str) -> Result<f64, ApiError> {",
+      "                match map.get(key) {",
+      "                    Some(v) => {",
+      "                        Ok(FromConvexValue::from_convex(v.clone())?)",
+      "                    }",
+      "                    _ => return Err(ApiError::ConvexClientError(format!(\"Expected field (f64) '{}' not found\", key))),",
+      "                }",
+      "            }",
+      "            fn get_project_id(map: &BTreeMap<String, Value>, key: &str) -> Result<Id<types::ProjectsDoc>, ApiError> {",
+      "                match map.get(key) {",
+      "                    Some(v) => {",
+      "                        Ok(FromConvexValue::from_convex(v.clone())?)",
+      "                    }",
+      "                    _ => return Err(ApiError::ConvexClientError(format!(\"Expected field (Id<types::ProjectsDoc>) '{}' not found\", key))),",
+      "                }",
+      "            }",
+      "            fn get_asset_name(map: &BTreeMap<String, Value>, key: &str) -> Result<String, ApiError> {",
+      "                match map.get(key) {",
+      "                    Some(v) => {",
+      "                        Ok(FromConvexValue::from_convex(v.clone())?)",
+      "                    }",
+      "                    _ => return Err(ApiError::ConvexClientError(format!(\"Expected field (String) '{}' not found\", key))),",
+      "                }",
+      "            }",
+      "            fn get_link_metadata(map: &BTreeMap<String, Value>, key: &str) -> Result<types::GetAssetsReturnLinkMetadataObject, ApiError> {",
+      "                match map.get(key) {",
+      "                    Some(v) => {",
+      "                        Ok(FromConvexValue::from_convex(v.clone())?)",
+      "                    }",
+      "                    _ => return Err(ApiError::ConvexClientError(format!(\"Expected field (types::GetAssetsReturnLinkMetadataObject) '{}' not found\", key))),",
+      "                }",
+      "            }",
+      "            fn get_asset_essence_mtime(map: &BTreeMap<String, Value>, key: &str) -> Result<i64, ApiError> {",
+      "                match map.get(key) {",
+      "                    Some(v) => {",
+      "                        Ok(FromConvexValue::from_convex(v.clone())?)",
+      "                    }",
+      "                    _ => return Err(ApiError::ConvexClientError(format!(\"Expected field (i64) '{}' not found\", key))),",
+      "                }",
+      "            }",
+      "            Ok(GetAssetsReturnObject {",
+      "                _id: get__id(&obj, \"_id\")?,",
+      "                _creation_time: get__creation_time(&obj, \"_creationTime\")?,",
+      "                project_id: get_project_id(&obj, \"project_id\")?,",
+      "                asset_name: get_asset_name(&obj, \"asset_name\")?,",
+      "                link_metadata: get_link_metadata(&obj, \"link_metadata\")?,",
+      "                asset_essence_mtime: get_asset_essence_mtime(&obj, \"asset_essence_mtime\")?",
+      "            })",
+      "        }",
+      "    }"
+    ]
+
 -- Main test suite
 tests :: Test
 tests =
@@ -87,7 +183,11 @@ tests =
       [ runSerializationTest
           "generates correct to_convex_value for complex nested objects"
           complexCreateFunction
-          expectedComplexSerializationImpl
+          expectedComplexSerializationImpl,
+        runSerializationTest
+          "generates correct from_convex for getAssets function"
+          getAssetsFunction
+          expectedGetAssetsSerializationImpl
       ]
 
 -- Helper to check for substring containment.
